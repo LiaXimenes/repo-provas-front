0dec30a619d8ebdd4be78101d2e2ca67f8c508d9
Lia Ximenes <ximeneslia@gmail.com>
2021-07-31 20:04:40 -0300

    seting teacher and subject rerender on app

--- src/components/SearchSubject.js
+++ src/components/SearchSubject.js
@@ -3,10 +3,50 @@ import axios from 'axios'
 import { useEffect, useState } from "react";
 
 export default function SearchSubject(){
-    const [subjects, setSubjects] = useState([])
+    const [subjects, setSubjects] = useState([]);
+    const [tests, setTests] = useState([]);
+
+    const [sem1, setSem1] = useState([]);
+    const [sem2, setSem2] = useState([]);
+    const [sem3, setSem3] = useState([]);
+    const [sem4, setSem4] = useState([]);
+    const [sem5, setSem5] = useState([]);
+    const [sem6, setSem6] = useState([]);
+    const [sem7, setSem7] = useState([]);
+    const [sem8, setSem8] = useState([]);
+
+    const [p1, setP1] = useState([]);
+    const [p2, setP2] = useState([]);
+    const [p3, setP3]= useState([]);
+    const [segChamada, setSegChamada] = useState([]);
+    const [outras, setOutras] = useState([]);
 
     useEffect(() => {
-        const request = axios.get("http://localhost:4000/search-subject");
+        for(let i = 0; i < subjects.length; i++){
+            if(subjects[i].semesterId === 1){
+                setSem1([...sem1, subjects[i]])
+            } else if(subjects[i].semesterId === 2){
+                setSem2([...sem2, subjects[i]])
+            } else if(subjects[i].semesterId === 3){
+                setSem3([...sem3, subjects[i]])
+            } else if(subjects[i].semesterId === 4){
+                setSem4([...sem4, subjects[i]])
+            } else if(subjects[i].semesterId === 5){
+                setSem5([...sem5, subjects[i]])
+            } else if(subjects[i].semesterId === 6){
+                setSem6([...sem6, subjects[i]])
+            } else if(subjects[i].semesterId === 7){
+                setSem7([...sem7, subjects[i]])
+            } else{
+                setSem8([...sem8, subjects[i]])
+            }
+
+            console.log(sem1)
+        }
+    }, [subjects])
+
+    useEffect(() => {
+        const request = axios.get("http://localhost:4000/search-test/subject");
         request.then((response) => {console.log(response.data); setSubjects(response.data)});
         request.catch(errors)
 
@@ -15,6 +55,36 @@ export default function SearchSubject(){
         }
     },[])
 
+    useEffect(() => {
+        for(let i = 0; i < tests.length; i++){
+            if(tests[i].categoryId === 1){
+                setP1([...p1, tests[i]]) 
+            } else if(tests[i].categoryId === 2){
+                setP2([...p2, tests[i]]) 
+            } else if(tests[i].categoryId === 3){
+                setP3([...p3, tests[i]]) 
+            } else if(tests[i].categoryId === 4){
+                setSegChamada([...segChamada, tests[i]]) 
+            } else{setOutras([...outras, tests[i]])}
+        }
+    }, [tests])
+
+    function findTestBySubjectId(id){
+        setP1([]);
+        setP2([]);
+        setP3([]);
+        setSegChamada([]);
+        setOutras([]);
+
+        const request = axios.get(`http://localhost:4000/search-test/subject/${id}`);
+        request.then((response) => {console.log(response.data); setTests(response.data)});
+        request.catch(errors)
+
+        function errors(error){
+
+        }
+    }
+
 
     return(
         <>
@@ -22,16 +92,88 @@ export default function SearchSubject(){
                 <p>Nessa página temos todas as disciplinas, escolhe uma que mostraremos as provas que temos</p>
             </Header>
 
-            <List>
+            <Body>
+                <List>
+                    {subjects.map((subject) => {
+                        return(
+                            <EachOption id={subject.id} onClick={() => findTestBySubjectId(subject.id)}>
+                                {subject.name}
+                            </EachOption>
+                        )
+                    })}
+                </List>
 
-                {subjects.map((subject) => {
-                    return(
-                        <EachOption id={subject.id}>
-                            {subject.name}
-                        </EachOption>
-                    )
-                })}
-            </List>
+                
+                {tests.length === 0 ? "" : 
+                    <List>
+                        {p1.length === 0 ? <p>Não temos nenhuma P1 :(</p> : 
+                            <Category>
+                                <p>P1</p>
+                                {p1.map((test) => {
+                                return(
+                                    <EachOption id={test.id}>
+                                    Prova: {test.name}
+                                    </EachOption>
+                                )
+                                })}
+                            </Category>
+                        }
+
+                        {p2.length === 0 ? <p>Não temos nenhuma P2 :(</p> : 
+                            <Category>
+                                <p>P2</p>
+                                {p2.map((test) => {
+                                return(
+                                    <EachOption id={test.id}>
+                                    Prova: {test.name}
+                                    </EachOption>
+                                )
+                                })}
+                            </Category>
+                        }
+
+                        {p3.length === 0 ? <p>Não temos nenhuma P3 :(</p> : 
+                            <Category>
+                                <p>P3</p>
+                                {p3.map((test) => {
+                                return(
+                                    <EachOption id={test.id}>
+                                    Prova: {test.name}
+                                    </EachOption>
+                                )
+                                })}
+                            </Category>
+                        }
+
+                        {segChamada.length === 0 ? "" : 
+                            <Category>
+                                <p>Segunda Chamada</p>
+                                {segChamada.map((test) => {
+                                return(
+                                    <EachOption id={test.id}>
+                                    Prova: {test.name}
+                                    </EachOption>
+                                )
+                                })}
+                            </Category>
+                        }
+
+                        {outras.length === 0 ? "" : 
+                            <Category>
+                                <p>P3</p>
+                                {outras.map((test) => {
+                                return(
+                                    <EachOption id={test.id}>
+                                    Prova: {test.name}
+                                    </EachOption>
+                                )
+                                })}
+                            </Category>
+                        }
+
+                    </List>
+                }
+            </Body>   
         </>
     )
 }
@@ -52,17 +194,26 @@ const Header = styled.div`
     }
 ` ;
 
-const List = styled.ul`
-    width: 100%;
+const Body = styled.div`
+    display: flex;
+`;
+
+const List = styled.div`
+    width: 50%;
     height: 400px;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: space-around;
-
 `;
 
-const EachOption = styled.li` 
+const Category = styled.div`
+    width: 50%;
+`;
+
+const EachOption = styled.button` 
+    background: white;
+    margin: 10px;
     :hover{
         cursor: pointer;
     }
--- src/components/SearchTeacher.js
+++ src/components/SearchTeacher.js
@@ -3,10 +3,16 @@ import axios from 'axios'
 import { useEffect, useState } from "react";
 
 export default function SearchTeacher(){
-    const [teachers, setTeachers] = useState([])
+    const [teachers, setTeachers] = useState([]);
+    const [tests, setTests] = useState([]);
+    const [p1, setP1] = useState([]);
+    const [p2, setP2] = useState([]);
+    const [p3, setP3]= useState([]);
+    const [segChamada, setSegChamada] = useState([]);
+    const [outras, setOutras] = useState([]);
 
     useEffect(() => {
-        const request = axios.get("http://localhost:4000/search-teacher");
+        const request = axios.get("http://localhost:4000/search-test/teacher");
         request.then((response) => {console.log(response.data); setTeachers(response.data)});
         request.catch(errors)
 
@@ -15,22 +21,123 @@ export default function SearchTeacher(){
         }
     },[])
 
+    useEffect(() => {
+        for(let i = 0; i < tests.length; i++){
+            if(tests[i].categoryId === 1){
+                setP1([...p1, tests[i]]) 
+            } else if(tests[i].categoryId === 2){
+                setP2([...p2, tests[i]]) 
+            } else if(tests[i].categoryId === 3){
+                setP3([...p3, tests[i]]) 
+            } else if(tests[i].categoryId === 4){
+                setSegChamada([...segChamada, tests[i]]) 
+            } else{setOutras([...outras, tests[i]])}
+        }
+    }, [tests])
+
+    function findTestByTeacherId(id){
+        setP1([]);
+        setP2([]);
+        setP3([]);
+        setSegChamada([]);
+        setOutras([]);
+
+        const request = axios.get(`http://localhost:4000/search-test/teacher/${id}`);
+        request.then((response) => {console.log(response.data); setTests(response.data)});
+        request.catch(errors)
+
+        function errors(error){
+
+        }
+    }
+
     return(
         <>
             <Header>
                 <p>Nessa página tem todos os professores, escolhe um que mostraremos as provas que temos</p>
             </Header>
 
-            <List>
-                {teachers.map((teacher) => {
-                    return (
-                    <EachOption id={teacher.id}>
-                        {teacher.name}
-                    </EachOption>
-                    )
-                })}
-            </List>
-    
+            <Body>
+                <List>
+                    {teachers.map((teacher) => {
+                        return (
+                        <EachOption id={teacher.id} onClick={() => findTestByTeacherId(teacher.id)}>
+                            {teacher.name}
+                        </EachOption>
+                        )
+                    })}
+                </List>
+
+                {tests.length === 0 ? "" : 
+                    <List>
+                        {p1.length === 0 ? <p>Não temos nenhuma P1 :(</p> : 
+                            <Category>
+                                <p>P1</p>
+                                {p1.map((test) => {
+                                return(
+                                    <EachOption id={test.id}>
+                                    Prova: {test.name}
+                                    </EachOption>
+                                )
+                                })}
+                            </Category>
+                        }
+
+                        {p2.length === 0 ? <p>Não temos nenhuma P2 :(</p> : 
+                            <Category>
+                                <p>P2</p>
+                                {p2.map((test) => {
+                                return(
+                                    <EachOption id={test.id}>
+                                    Prova: {test.name}
+                                    </EachOption>
+                                )
+                                })}
+                            </Category>
+                        }
+
+                        {p3.length === 0 ? <p>Não temos nenhuma P3 :(</p> : 
+                            <Category>
+                                <p>P3</p>
+                                {p3.map((test) => {
+                                return(
+                                    <EachOption id={test.id}>
+                                    Prova: {test.name}
+                                    </EachOption>
+                                )
+                                })}
+                            </Category>
+                        }
+
+                        {segChamada.length === 0 ? "" : 
+                            <Category>
+                                <p>Segunda Chamada</p>
+                                {segChamada.map((test) => {
+                                return(
+                                    <EachOption id={test.id}>
+                                    Prova: {test.name}
+                                    </EachOption>
+                                )
+                                })}
+                            </Category>
+                        }
+
+                        {outras.length === 0 ? "" : 
+                            <Category>
+                                <p>P3</p>
+                                {outras.map((test) => {
+                                return(
+                                    <EachOption id={test.id}>
+                                    Prova: {test.name}
+                                    </EachOption>
+                                )
+                                })}
+                            </Category>
+                        }
+
+                    </List>
+                }
+            </Body>
         </>
     )
 }
@@ -51,16 +158,25 @@ const Header = styled.div`
     }
 ` ;
 
-const List = styled.ul`
-    width: 100%;
-    height: 400px;
+const Body = styled.div`
+    display: flex;
+`;
+
+const List = styled.div`
+    width: 50%;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: space-around;
 `;
 
-const EachOption = styled.li` 
+const Category = styled.div`
+    width: 50%;
+`;
+
+const EachOption = styled.button` 
+    background: white;
+    margin: 10px;
     :hover{
         cursor: pointer;
     }

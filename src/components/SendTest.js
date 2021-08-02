c17ba06bbc3a0823b929e81f2fc15659394fc624
Lia Ximenes <ximeneslia@gmail.com>
2021-08-02 13:54:02 -0300

    changes to deploy

--- src/components/SendTest.js
+++ src/components/SendTest.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import axios from 'axios'
 import { useEffect, useState } from "react";
 import {useHistory} from "react-router-dom";
 import {api} from "../services/api";
